83ce119b3687fd4d384e6d704b223b112c42c253
Mallepally Lokeshwar Reddy <lokeshwar777@gmail.com>
2024-07-07 19:00:11 +0530

    chore: Register HeroReview, Store, and HeroCertificate models in the admin site

--- lokiDjango/hero/admin.py
+++ lokiDjango/hero/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import HeroType
+from .models import HeroType, HeroReview, Store, HeroCertificate
 # Register your models here.
 
-admin.site.register(HeroType)
+class HeroReviewInline(admin.TabularInline):
+    model = HeroReview
+    extra = 2
+
+class HeroTypeAdmin(admin.ModelAdmin):
+    list_display = ('name','type','date_added')
+    inlines = [HeroReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name','location')
+    filter_horizontal = ('hero_types')
+
+class HeroCertificateAdmin(admin.ModelAdmin):
+    list_display= ('hero', 'ceritficate_number')
+
+admin.site.register(HeroType, HeroTypeAdmin)
+admin.site.register(HeroReview, HeroReviewInline)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(HeroCertificate, HeroCertificateAdmin)
--- lokiDjango/hero/models.py
+++ lokiDjango/hero/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 # Create your models here.
 class HeroType(models.Model):
     HERO_TYPE_CHOICE = [
@@ -16,3 +18,37 @@ class HeroType(models.Model):
 
 def __str__(self):
     return self.name
+
+
+# one to many
+
+class HeroReview(models.Model):
+    hero = models.ForeignKey(HeroType, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} review fro {self.hero.name}'
+
+# many to many
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    hero_types = models.ManyToManyField(HeroType, related_name='stores')
+
+    def __str__(self):
+        return self.name
+
+# one to one 
+
+class HeroCeritficate(models.Model):
+    hero = models.OneToOneField(HeroType, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issued_date = models.DateTimeField(default=timezone.now)
+    valid_until = models.DateTimeField()
+
+    def __str__(self):
+        return f'Certificate for {self.name.hero}'
+    
